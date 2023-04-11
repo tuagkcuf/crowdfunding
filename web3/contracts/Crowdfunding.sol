a9dcf3e26a60b04548fbd4b8d38bd146850ebc31
tuagkcuf <jokersms222@gmail.com>
2023-04-11 12:47:04 +0300

    specifying contract structure

--- web3/contracts/Crowdfunding.sol
+++ web3/contracts/Crowdfunding.sol
@@ -3,6 +3,26 @@ pragma solidity ^0.8.9;
 
 contract Crowdfunding {
     struct Campaign {
-        
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
     }
+
+    mapping (uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public {}
+
+    function donateToCampaign() public {}
+
+    function getDonators() public {}
+
+    function getCampaigns() public {}
 }
