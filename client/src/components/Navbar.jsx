fa4057cb2b5823aedd81fdc307c0043202059629
tuagkcuf <jokersms222@gmail.com>
2023-04-12 18:16:33 +0300

    add navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
     const [isActive, setIsActive] = useState("dashboard");
     const [toggleDrawer, setToggleDrawer] = useState(false);
 
+    const address = "0x...sdf";
+
     return (
         <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
             <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
@@ -25,6 +27,28 @@ const Navbar = () => {
                         className="w-[15px] h-[15px] object-contain"
                     />
                 </div>
+            </div>
+
+            <div className="sm:flex hidden flex-row justify-end gap-4">
+                <CustomButton
+                    btnType="button"
+                    title={address ? "Create a campaign" : "Connect"}
+                    styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+                    handleClick={() => {
+                        if (address) navigate("create-campaign");
+                        else "connect()";
+                    }}
+                />
+
+                <Link to="profile">
+                    <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+                        <img
+                            src={thirdweb}
+                            alt="user"
+                            className="w-[60%] h-[60%] object-contain"
+                        />
+                    </div>
+                </Link>
             </div>
         </div>
     );
