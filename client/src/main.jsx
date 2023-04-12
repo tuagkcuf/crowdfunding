4170206b4834bd7622e0ef1e51589d79b42875b0
tuagkcuf <jokersms222@gmail.com>
2023-04-12 16:19:03 +0300

    minor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./index.css"
 
 import App from "./App";
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
