ed5dbdc32ae0da2851355c78f1ebf03ec03907f2
tuagkcuf <jokersms222@gmail.com>
2023-04-12 12:41:45 +0300

    minor changes

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,7 +1,25 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const SEPOLIA_RPC_URL=process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: "sepolia",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+    },
     settings: {
       optimizer: {
         enabled: true,
