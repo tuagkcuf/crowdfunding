68f73497b7bcf3229f46af62c710592e1bb07a60
tuagkcuf <jokersms222@gmail.com>
2023-04-13 18:44:14 +0300

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,9 +13,15 @@ const App = () => {
 
             <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
                 <Navbar />
-                
+
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+                    <Route
+                        path="/createa-campaign"
+                        element={<CreateCampaign />}
+                    />
                 </Routes>
             </div>
         </div>
