715c7fd01c8e60ae51d240258ca3e2ba740b3209
tuagkcuf <jokersms222@gmail.com>
2023-04-14 16:24:14 +0300

    add context to the app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 
+import { StateContextProvider } from "./context";
 import App from "./App";
-import "./index.css"
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <ThirdwebProvider desiredChainId={11155111}>
         <Router>
-            <App />
+            <StateContextProvider>
+                <App />
+            </StateContextProvider>
         </Router>
     </ThirdwebProvider>
 );
